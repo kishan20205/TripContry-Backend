728a6d7d93b1439ace4cf2af94ec965127b84ede
kishan20205 <kishansavaliya258@gmail.com>
2026-01-13 10:18:40 +0530

    First

--- src/main/java/com/tripContry/TripContryManagement/repositories/TripRepository.java
+++ src/main/java/com/tripContry/TripContryManagement/repositories/TripRepository.java
@@ -3,8 +3,6 @@ package com.tripContry.TripContryManagement.repositories;
 import com.tripContry.TripContryManagement.entities.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
 }
